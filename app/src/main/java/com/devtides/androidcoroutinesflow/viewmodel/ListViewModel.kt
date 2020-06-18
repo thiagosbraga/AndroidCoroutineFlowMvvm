ef68a7ea10c70235b47d306f2dbee508f4a75b4a
Thiago Braga <thiagosilvabraga@gmail.com>
2020-06-17 22:06:04 -0300

    implement coroutines flows to get data from a server using reterofit and mvvm pattern

--- app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
@@ -2,9 +2,12 @@ package com.devtides.androidcoroutinesflow.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.devtides.androidcoroutinesflow.model.NewsRepository
+import com.devtides.androidcoroutinesretrofit.model.NewsArticle
 
 class ListViewModel: ViewModel() {
 
-    val newsArticles = MutableLiveData<String>()
+    val newsArticles = NewsRepository().getNewsArticle().asLiveData()
 
 }
